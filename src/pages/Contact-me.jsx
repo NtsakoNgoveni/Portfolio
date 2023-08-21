1d34d8056136d62c8573977d10e3c1ef59e335bd
Ntsako Ngoveni <naction923@gmail.com>
2023-08-21 14:35:40 +0200

    email update

--- src/pages/Contact-me.jsx
+++ src/pages/Contact-me.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div name="contact-me" className="contact">
             <h1>Contact me</h1>
-            <form className="contact-form" action="https://formsubmit.co/ntsakon3@gmail" method="POST">
+            <form className="contact-form" action="https://formsubmit.co/ntsakon3@gmail.com" method="POST">
                 <input className="contact-input" name="name" type="text" placeholder="Enter your name" />
                 <input className="contact-input" name="email" type="email" placeholder="Enter your email" />
                 <textarea className="contact-textarea" name="message" placeholder="Write your message"></textarea>
